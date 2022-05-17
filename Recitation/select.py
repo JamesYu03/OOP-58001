f3428e4d82fcacddc9c68e5dc9c421afd694e166
JamesYu03 <98925351+JamesYu03@users.noreply.github.com>
2022-05-17 18:53:40 +0800

    Add files via upload

---
+++ Recitation/select.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+try:
+    connect= pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\Users\Yu\Downloads\Database1.accdb;')
+    print("Connected to a Database")
+
+    record = connect.cursor()
+    record.execute('select * from Table1')
+    for row in record.fetchall():
+        print(row)
+
+
+
+except pyodbc.Error as e:
+    print("Error in Connection")
